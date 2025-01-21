2132154a317a25dff9a825f0918f7544e0b6fb3c
Storming <64513117+dw8869@users.noreply.github.com>
2025-01-21 12:34:24 +0900

    -

--- Core.Bridge/Runtime/Scripts/Internal/Interface/INativeBridge.cs
+++
@@ -1,8 +0,0 @@
-namespace Core.Bridge
-{
-    internal interface INativeBridge
-    {
-        void Initialize(BridgeProvider.MessageCallback callback);
-        void CallNative(string eventName, string jsonData);
-    }
-}
